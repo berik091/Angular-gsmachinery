f4738eaca1f3149354e03baa28e4ad5bc03700d5
Berik <53177870+berik091@users.noreply.github.com>
2020-03-07 20:58:29 +0600

    change v 1.0

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,101 +7,237 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
   active = 1;
-  @Input() product;
-  @Input() vertical;
-  products: any[] = [
-    // Horizontal feeder mixer
-    // 1
-    {
-      name: 'Fibre Chair',
+  @Input() furniture;
+  @Input() decorative;
+  @Input() lighting;
+  @Input() outdoor;
+  @Input() storage;
+
+    furnitures: any[]= [
+      {
+      id:1,
+      name: "Classic Wardrobe",
       imageUrl: 'assets/images/product/p-1.jpg',
-      discount: '-59%',
-      price: '50$'
-    },
-    // 2
-    {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
       discount: '-50%',
-      price: '59$'
+      price: '40$'
     },
-    // 3
     {
-      name: 'Classic Wardrobe',
+      id:2,
+      name: "Touchwood Chaira",
+      imageUrl: 'assets/images/product/p-2.jpg',
+      discount: '-55%',
+      price: '129$'
+    },
+    {
+      id:3,
+      name: "Classic Wardrobe",
       imageUrl: 'assets/images/product/p-3.jpg',
-      discount: '-10%',
-      price: '89$'
+      discount: '-60%',
+      price: '49$'
     },
-    // 4
     {
-      name: 'Touchwood Chair 2',
+      id:4,
+      name: "Classic Wardrobe",
       imageUrl: 'assets/images/product/p-2.jpg',
       discount: '-50%',
       price: '59$'
     },
-    // 5
     {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
-      discount: '-50%',
+      id:5,
+      name: "Touchwood Chaira",
+      imageUrl: 'assets/images/product/p-3.jpg',
+      discount: '-55%',
       price: '59$'
     },
-    // 6
     {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
-      discount: '-50%',
-      price: '59$'
+      id:6,
+      name: "Classic Wardrobe",
+      imageUrl: 'assets/images/product/p-1.jpg',
+      discount: '-60%',
+      price: '79$'
     },
   ];
-  verticals: any[] = [
-    // Horizontal feeder mixer
-    // 1
-    {
-      name: 'Fibre Chair',
-      imageUrl: 'assets/images/product/p-1.jpg',
-      discount: '-80%',
-      price: '50$'
-    },
-    // 2
-    {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
-      discount: '-50%',
-      price: '59$'
-    },
-    // 3
-    {
-      name: 'Classic Wardrobe',
-      imageUrl: 'assets/images/product/p-3.jpg',
-      discount: '-10%',
-      price: '89$'
-    },
-    // 4
-    {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
-      discount: '-50%',
-      price: '59$'
-    },
-    // 5
-    {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
-      discount: '-50%',
-      price: '59$'
-    },
-    // 6
-    {
-      name: 'Touchwood Chair 2',
-      imageUrl: 'assets/images/product/p-2.jpg',
-      discount: '-50%',
-      price: '59$'
-    },
-  ];
+
+    decoratives:any[]=[
+      {
+        id:1,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-10%',
+        price: '50$'
+      },
+      {
+        id:2,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-55%',
+        price: '129$'
+      },
+      {
+        id:3,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-60%',
+        price: '49$'
+      },
+      {
+        id:4,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-50%',
+        price: '59$'
+      },
+      {
+        id:5,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-55%',
+        price: '59$'
+      },
+      {
+        id:6,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-1.jpg',
+        discount: '-60%',
+        price: '79$'
+      },
+    ]
+
+    lightings:any[]=[
+      {
+        id:1,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-100%',
+        price: '50$'
+      },
+      {
+        id:2,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-55%',
+        price: '129$'
+      },
+      {
+        id:3,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-60%',
+        price: '49$'
+      },
+      {
+        id:4,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-50%',
+        price: '59$'
+      },
+      {
+        id:5,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-55%',
+        price: '59$'
+      },
+      {
+        id:6,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-1.jpg',
+        discount: '-60%',
+        price: '79$'
+      },
+    ]
+
+    outdoors:any[]=[
+      {
+        id:1,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-1.jpg',
+        discount: '-90%',
+        price: '50$'
+      },
+      {
+        id:2,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-55%',
+        price: '129$'
+      },
+      {
+        id:3,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-60%',
+        price: '49$'
+      },
+      {
+        id:4,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-50%',
+        price: '59$'
+      },
+      {
+        id:5,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-55%',
+        price: '59$'
+      },
+      {
+        id:6,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-1.jpg',
+        discount: '-60%',
+        price: '79$'
+      },
+    ]
+    storages:any[]=[
+      {
+        id:1,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-1.jpg',
+        discount: '-90%',
+        price: '50$'
+      },
+      {
+        id:2,
+        name: "Touchwood",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-20%',
+        price: '19$'
+      },
+      {
+        id:3,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-60%',
+        price: '49$'
+      },
+      {
+        id:4,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-2.jpg',
+        discount: '-50%',
+        price: '59$'
+      },
+      {
+        id:5,
+        name: "Touchwood Chaira",
+        imageUrl: 'assets/images/product/p-3.jpg',
+        discount: '-55%',
+        price: '59$'
+      },
+      {
+        id:6,
+        name: "Classic Wardrobe",
+        imageUrl: 'assets/images/product/p-1.jpg',
+        discount: '-60%',
+        price: '79$'
+      },
+    ]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
   }
-}
