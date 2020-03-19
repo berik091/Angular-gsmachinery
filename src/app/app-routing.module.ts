647b269d76585b07c6a674494cff83d86305229c
Berik <berik9105@gmail.com>
2020-03-19 16:48:30 +0600

    add products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,17 +7,20 @@ import { ProductsComponent } from './components/products/products.component';
 import { ServiceComponent } from './components/service/service.component';
 import { TeamComponent } from './components/team/team.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { DiscountComponent } from './components/discount/discount.component';
 import { NoutfoundcomponentComponent } from './noutfoundcomponent/noutfoundcomponent.component';
+import { FormComponent } from './components/form/form.component';
+import { DiscountComponent } from './components/discount/discount.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: BssliderComponent},
+  { path: 'Home', component: DiscountComponent},
   { path: 'Product', component: ProductsComponent },
   { path: 'Service', component: ServiceComponent },
   { path: 'Team', component: TeamComponent },
   { path: 'Contact', component: ContactComponent },
   { path: '404', component: NoutfoundcomponentComponent },
+  { path: 'Form', component: FormComponent },
   { path: '**', redirectTo: '/404' }
 ];
 
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,60 +10,48 @@ export class ProductsComponent implements OnInit {
   public page: number;
   public collectionSize: number;
   active = 1;
-  @Input() furniture;
-  @Input() decorative;
-  @Input() lighting;
-  @Input() outdoor;
-  @Input() storage;
+  @Input() horizontal: any;
+  @Input() vertical: any;
+  @Input() auger;
+  @Input() silage;
+  @Input() hale;
 
-furnitures: any[] = [
+  horizontals: any[] = [
 
-  { id: 1, name: 'HORIZONTAL FEED MIXER 2.5 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor1.jpg', discount: '-50%', price: 'KZT 990,000.00'},
-  { id: 2, name: 'HORIZONTAL FEED MIXER 4 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor2.jpeg', discount: '-55%', price: 'KZT 990,000.00'},
-  { id: 3, name: 'HORIZONTAL FEED MIXER 5 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor3.jpg', discount: '-60%', price: 'KZT 990,000.00'},
-  { id: 4, name: 'HORIZONTAL FEED MIXER 6 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor4.jpg', discount: '-50%', price: 'KZT 990,000.00' },
-  { id: 5, name: 'HORIZONTAL FEED MIXER 8 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor5.jpg', discount: '-50%', price: 'KZT 990,000.00'},
-  { id: 6, name: 'HORIZONTAL FEED MIXER 10 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor6.jpeg', discount: '-55%', price: 'KZT 990,000.00'},
-  { id: 7, name: 'HORIZONTAL FEED MIXER 12 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor7.jpg', discount: '-60%', price: 'KZT 990,000.00'},
-  { id: 8, name: 'HORIZONTAL FEED MIXER 14 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor8.jpg', discount: '-50%', price: 'KZT 990,000.00' },
-  { id: 9, name: 'HORIZONTAL FEED MIXER 16 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor9.jpg', discount: '-50%', price: 'KZT 990,000.00' },
-  { id: 9, name: 'HORIZONTAL FEED MIXER 20 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor10.jpg', discount: '-50%', price: 'KZT 990,000.00' },
+  { id: 1, name: 'HORIZONTAL FEED MIXER 2.5 M3',imageUrl: 'assets/images/product/horizontalfeedmixer/hor1.jpg', price: 'KZT 990,000.00'},
+  { id: 2, name: 'HORIZONTAL FEED MIXER 4 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor2.jpeg', price: 'KZT 990,000.00'},
+  { id: 3, name: 'HORIZONTAL FEED MIXER 5 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor3.jpg',  price: 'KZT 990,000.00'},
+  { id: 4, name: 'HORIZONTAL FEED MIXER 6 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor4.jpg',  price: 'KZT 990,000.00' },
+  { id: 5, name: 'HORIZONTAL FEED MIXER 8 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor5.jpg',  price: 'KZT 990,000.00'},
+  { id: 6, name: 'HORIZONTAL FEED MIXER 10 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor6.jpeg', price: 'KZT 990,000.00'},
+  { id: 7, name: 'HORIZONTAL FEED MIXER 12 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor7.jpg',  price: 'KZT 990,000.00'},
+  { id: 8, name: 'HORIZONTAL FEED MIXER 14 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor8.jpg',  price: 'KZT 990,000.00' },
+  { id: 9, name: 'HORIZONTAL FEED MIXER 16 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor9.jpg',  price: 'KZT 990,000.00' },
+  { id: 9, name: 'HORIZONTAL FEED MIXER 20 M3', imageUrl: 'assets/images/product/horizontalfeedmixer/hor10.jpg', price: 'KZT 990,000.00' },
   ];
 
-decoratives: any[] = [
-  { id: 1, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 2, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'},
-  { id: 3, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-3.jpg', discount: '-60%', price: '49$'},
-  { id: 4, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-2.jpg', discount: '-50%', price: '59$' },
-  { id: 5, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 6, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'}
+verticals: any[] = [
+  { id: 1, name: 'VERTICAL FEED MIXER 1.5 M3', imageUrl: 'assets/images/product/verticalfeedmixer/ver1.jpg', price: 'KZT 990,000.00'},
+  { id: 2, name: 'VERTICAL FEED MIXER 3 M3', imageUrl: 'assets/images/product/verticalfeedmixer/ver2.jpg',  price: 'KZT 990,000.00'},
+  { id: 3, name: 'VERTICAL FEED MIXER 5 M3', imageUrl: 'assets/images/product/verticalfeedmixer/ver3.jpg', price: 'KZT 990,000.00'},
+  { id: 4, name: 'VERTICAL FEED MIXER 8 M3', imageUrl: 'assets/images/product/verticalfeedmixer/ver4.jpg', price: 'KZT 990,000.00' },
+  { id: 5, name: 'VERTICAL FEED MIXER 12 M3', imageUrl: 'assets/images/product/verticalfeedmixer/ver5.jpg', price: 'KZT 990,000.00'},
+  { id: 6, name: 'VERTICAL FEED MIXER 20 M3', imageUrl: 'assets/images/product/verticalfeedmixer/ver6.jpg', price: 'KZT 990,000.00'}
 ];
 
-lightings: any[] = [
-  { id: 1, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 2, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'},
-  { id: 3, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-3.jpg', discount: '-60%', price: '49$'},
-  { id: 4, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-2.jpg', discount: '-50%', price: '59$' },
-  { id: 5, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 6, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'}
+augers: any[] = [
+  { id: 1, name: 'SINGLE AUGER HORIZANTAL FEED MIXER 4 M3', imageUrl: 'assets/images/product/Single Auger Horizantal Feed Mixer/single-hor01.jpg', price: 'KZT 990,000.00'},
+  { id: 2, name: 'SINGLE AUGER HORIZANTAL FEED MIXER 6 M3', imageUrl: 'assets/images/product/Single Auger Horizantal Feed Mixer/single-hor02.jpg', price: 'KZT 990,000.00'},
 ];
 
-outdoors: any[] = [
-  { id: 1, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-5%', price: '40$'},
-  { id: 2, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'},
-  { id: 3, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-3.jpg', discount: '-60%', price: '49$'},
-  { id: 4, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-2.jpg', discount: '-50%', price: '59$' },
-  { id: 5, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 6, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'},
+silages: any[] = [
+  { id: 1, name: 'MOBILE SILAGE ROUND BALER', imageUrl: 'assets/images/product/silage round baler/silage01.jpg', price: 'KZT 990,000.00'},
+  { id: 2, name: 'COMBINE SILAGE ROUND BALER', imageUrl: 'assets/images/product/silage round baler/silage02.jpg', price: 'KZT 990,000.00'}
 ];
 
-storages: any[] = [
-  { id: 1, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 2, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'},
-  { id: 3, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-3.jpg', discount: '-60%', price: '49$'},
-  { id: 4, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-2.jpg', discount: '-50%', price: '59$' },
-  { id: 5, name: 'Classic Wardrobe', imageUrl: 'assets/images/product/p-5.jpg', discount: '-50%', price: '40$'},
-  { id: 6, name: 'Touchwood Chaira', imageUrl: 'assets/images/product/p-2.jpg', discount: '-55%', price: '129$'},
+hales: any[] = [
+  { id: 1, name: 'Classic Hale Bopp-300 Rake-Ted Combination', imageUrl: 'assets/images/product/Hale bob/hale-bopp01.jpg', price: 'KZT 990,000.00'},
+
 ];
   constructor() {
    }
